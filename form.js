244be23cef226ccb9ed83c5d570283014a0a2c93
Leona-cloud <60227761+Leona-cloud@users.noreply.github.com>
2021-08-19 12:17:05 +0100

    Update form.js

--- form.js
+++ form.js
@@ -49,7 +49,7 @@ app.post('/sign_up', async(req, res)=>{
       console.log(result)
   }
 catch (ex) {
-    res.send('Please fill required fields');
+    res.send('Thanks for your response');
     console.log(ex.message);
 };
   
