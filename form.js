cdaf3abd22a9adfc0d043d669ce17187f1d186e1
Leona-cloud <60227761+Leona-cloud@users.noreply.github.com>
2021-08-19 09:39:22 +0100

    Add files via upload

---
+++ form.js
@@ -0,0 +1,68 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const bodyParser = require('body-parser');
+const Joi = require('joi');
+
+
+dotenv.config();
+
+const app = express();
+app.use(express.static('Routes'))
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+mongoose.connect(process.env.DB_CONNECT, 
+{useNewUrlParser: true,
+    useUnifiedTopology: true});
+
+var db = mongoose.connection;
+
+const regSchema = new mongoose.Schema({
+    name: { type : String, required: true},
+    email:  {type : String, required: true},
+    subject: {type : String, required: true},
+    messages: {type: String, required: true},
+    
+});
+const User = mongoose.model('User', regSchema);
+
+db.once('open', ()=> console.log('connected to the database'));
+   
+app.post('/sign_up', async(req, res)=>{
+    const user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        subject: req.body.subject,
+        messages: req.body.messages
+       
+    });
+    
+
+   
+  try{
+      const result = await user.save();
+      res.send('<p>Thanks for your response</p>')
+      console.log(result)
+  }
+catch (ex) {
+    res.send('Please fill required fields');
+    console.log(ex.message);
+};
+  
+});
+
+
+
+
+app.get('/', (req, res)=>{
+    res.set({
+        "Allow-access-Allow-origin": '*'
+    })
+    return res.redirect('form.html');
+}).listen(3000)
+
+console.log("connected to the server")
